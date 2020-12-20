2fbae8592593544503241d57573a9da6f9f8239c
watarona <nakagawatakuro0330@gmail.com>
2020-12-20 20:41:42 +0900

    add numpy

---
+++ numpy/array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+L = list(range(10))          #############
+print(L)                     #############
+L2 = [str(c) for c in L]     #python code#
+print(L2)                    #############
+
+import array
+A = array.array('i', 1)      #############
+pritn(A)                     #############
